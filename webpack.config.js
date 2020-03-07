8d04269c66d8b7ce77a19f61be188b8a431d0398
Martin Tudor <martcan27@gmail.com>
2020-03-07 17:27:15 +0000

    use devServer error overlay

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         path: path.resolve(__dirname, "docs"),
         filename: "bundle.js",
     },
+    devServer: {
+        overlay: true,
+    },
     module: {
         rules: [
             {
