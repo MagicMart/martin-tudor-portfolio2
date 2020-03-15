2a8150d501d28760ff39b41fea5a50713903f5ee
Martin Tudor <martcan27@gmail.com>
2020-03-15 21:15:25 +0000

    update image source

--- src/index.js
+++ src/index.js
@@ -5,42 +5,42 @@ document.querySelector("body").style.visibility = "visible";
 const featuredArr = [
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/memory-cards.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/memory-cards.jpg",
         alt: "Memory game cards - with React.js",
         link: "https://marts-memory-cards.netlify.com/",
         title: "Memory Game Cards",
     },
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/sequence2.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/sequence2.jpg",
         alt: "Sequence memory game",
         link: "https://sequence-game.netlify.com/",
         title: "Sequence",
     },
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/simple-blogger.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/simple-blogger.jpg",
         alt: "Simple Blogger front page",
         link: "https://simple-blogger.netlify.com/",
         title: "Simple Blogger",
     },
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/weather-app.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/weather-app.jpg",
         alt: "Still from weather app",
         link: "https://martin-weather-app.netlify.com/",
         title: "Weather App",
     },
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/hacker-news.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/hacker-news.jpg",
         alt: "Still showing posts from hacker news",
         link: "https://marts-hacker-news.netlify.com/",
         title: "Hacker News",
     },
     {
         image:
-            "https://martin-portfolio.s3-us-west-2.amazonaws.com/game-of-life.jpg",
+            "https://marts-portfolio.s3.eu-west-2.amazonaws.com/game-of-life.jpg",
         alt: "A still image from The Game Of life app",
         link: "https://magicmart.github.io/Game-of-Life/",
         title: "Game of Life",
